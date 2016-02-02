48493dc8a54b0fc7dfc0a2d804fa5723526635dd
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-02-02 02:40:34 -0500

    created abstract action class
    gotta streamline dat bitch

---
+++ DemonGymnasium/Assets/Scripts/entities/ActionTypes/Actions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Actions : MonoBehaviour
+{
+    public int turnCost = 1;
+    public string actionName = "Action";
+    Entity entity;
+
+    public abstract void performAction();
+
+    public void setEntity(Entity entitySelected)
+    {
+        this.entity = entitySelected;
+    }
+
+
+    public abstract void OnActionClicked();
+
+    public abstract Vector2[] legalActions();
+}
