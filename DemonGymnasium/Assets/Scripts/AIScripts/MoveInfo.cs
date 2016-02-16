000da564b057beb3b09541660b38504db3dd55a4
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-02-15 23:47:11 -0500

    MoveInfo added for AI

---
+++ DemonGymnasium/Assets/Scripts/AIScripts/MoveInfo.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveInfo : MonoBehaviour {
+    public Entity entity;
+    public Point2 tilePositionSelected;
+    public int actionSelected;
+    public int actionValue;
+}
