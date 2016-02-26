7b13575d32b6e836f40867bde6cc86137bf8b2d0
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-02-26 13:16:30 -0500

    Some touch screen methods

---
+++ DemonGymnasium/Assets/Scripts/TouchScreen/InputTouchScreen.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputTouchScreen : MonoBehaviour {
+    static Vector2[] touchPositions = new Vector2[5];
+    static Vector2[] oldTouchPositions = new Vector2[5];
+
+    void Update()
+    {
+        foreach(Touch t in Input.touches)
+        {
+            touchPositions[t.fingerId] = t.position;
+        }
+    }
+
+
+    public static float getInputSeparation()
+    {
+        if (Input.touchCount != 2)
+        {
+            return 0;
+        }
+        else
+        {
+            Touch[] allTouches = Input.touches;
+            foreach (Touch t in allTouches)
+            {
+                
+            }
+            return 0;
+        }
+    }
+
+    public static float getDragRate()
+    {
+        return 0;
+    }
+}
