6dc6b845852c47c299251123ca14b736df690dbc
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-02-26 14:58:41 -0500

    MapInfo for AI updated

--- DemonGymnasium/Assets/Scripts/AIScripts/MapInfoAI.cs
+++ DemonGymnasium/Assets/Scripts/AIScripts/MapInfoAI.cs
@@ -2,114 +2,87 @@
 using System.Collections;
 
 public class MapInfoAI : MonoBehaviour {
-
-    public const int JANITOR = 0;
-    public const int DEMON = 1;
-    public const int NEUTRAL = 2;
+    public const int JANITOR_OWNED = 0;
+    public const int DEMON_OWNED = 1;
+    public const int NEUTRAL_OWNED = 2;
     public const int JANITOR_PAWN = 3;
-    public const int DEMON_PAWN = 4;
-    public const int JANITOR_KING = 5;
+    public const int JANITOR_KING = 4;
+    public const int DEMON_PAWN = 5;
     public const int DEMON_KING = 6;
 
-    AIStateMachine aiStateMachine;
-    int[,] mapInfo;
-    float[,] mapWeights;
+    static int[,] currentMapLayout;
+    static int[,] alteredMapLayout;
 
     void Start()
     {
-        mapInfo = new int[MapGenerator.BoardWidth, MapGenerator.BoardHeight];
-        mapWeights = new float[MapGenerator.BoardWidth, MapGenerator.BoardHeight];
-        updateMapProperties();
+        currentMapLayout = new int[MapGenerator.BoardWidth, MapGenerator.BoardHeight];
+        alteredMapLayout = new int[MapGenerator.BoardWidth, MapGenerator.BoardHeight];
     }
 
-    public void updateMapProperties()
+    public static void updateCurrentMapLayout()
     {
-        for (int x = 0; x < mapInfo.GetLength(0); x++)
+        Tile tileAtPoint = null;
+        for (int x = 0; x < currentMapLayout.GetLength(0); x++)
         {
-            for (int y = 0; y < mapInfo.GetLength(1); y++)
+            for (int y = 0; y < currentMapLayout.GetLength(1); y++)
             {
-                Tile tileAtPoint = MapGenerator.getTileAtPoint(x, y);
-                if (tileAtPoint.getCurrentEntity() == null)
-                {
-                    mapInfo[x, y] = tileAtPoint.currentTileType;
-                }
-                else
+                tileAtPoint = MapGenerator.getTileAtPoint(x, y);
+                if (tileAtPoint.getCurrentEntity() != null)
                 {
                     Entity checkEntity = tileAtPoint.getCurrentEntity();
                     if (checkEntity is King)
                     {
                         if (checkEntity.entityType == GameManager.JANITOR)
                         {
-                            mapInfo[x, y] = JANITOR_KING;
+                            currentMapLayout[x, y] = JANITOR_KING;
                         }
                         else
                         {
-                            mapInfo[x, y] = DEMON_KING;
+                            currentMapLayout[x, y] = DEMON_KING;
                         }
                     }
-                    else
+                    else if (checkEntity is Minion)
                     {
-                        if (checkEntity.entityType == GameManager.DEMON)
+                        if (checkEntity.entityType == GameManager.JANITOR)
                         {
-                            mapInfo[x, y] = JANITOR_PAWN;
+                            currentMapLayout[x, y] = JANITOR_PAWN;
                         }
                         else
                         {
-                            mapInfo[x, y] = DEMON_PAWN;
+                            currentMapLayout[x, y] = DEMON_PAWN;
                         }
                     }
                 }
+                else
+                {
+                    currentMapLayout[x, y] = tileAtPoint.currentTileType;
+                }
+            }
+        }
+        resetAlteredToCurrentState();
+    }
+
+
+    public static void changeTileProperty (int x, int y, int property)
+    {
+        alteredMapLayout[x, y] = property;
+    }
+
+    public static void changeTileProperty(Point2 point, int property)
+    {
+        changeTileProperty(point.x, point.y, property);
+    }
+
+    public static void resetAlteredToCurrentState()
+    {
+        for (int x = 0; x < currentMapLayout.GetLength(0); x++)
+        {
+            for (int y = 0; y < currentMapLayout.GetLength(1); y++)
+            {
+                changeTileProperty(x, y, currentMapLayout[x, y]);
             }
         }
     }
 
 
-    public float getWeightAtLocation (Point2 tilePosition)
-    {
-        int tileValue = mapInfo[tilePosition.x, tilePosition.y];
-        int weight = 0;
-        if (tileValue == aiStateMachine.aiTeam)
-        {
-            weight += 0;
-        }
-        else if (tileValue == aiStateMachine.enemyTeam)
-        {
-            weight += 3;
-        }
-        else if (tileValue == NEUTRAL)
-        {
-            weight += 1;
-        }
-        
-        if (tileValue == getEnemyID("King"))
-        {
-            weight += 50;
-        }
-        else if (tileValue == getEnemyID("Pawn"))
-        {
-            weight += 15;
-        }
-
-
-        return weight;
-    }
-
-    int getEnemyID(string type)
-    {
-        switch (type)
-        {
-            case "King":
-                return JANITOR_KING;
-            case "Pawn":
-                return JANITOR_PAWN;
-            default:
-                return -1;
-        }
-
-    }
-
-    public float getWeightAtLocation(int x, int y)
-    {
-        return getWeightAtLocation(new Point2(x, y));
-    }
 }
