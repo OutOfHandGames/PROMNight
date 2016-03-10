96702109d674c00ffe1bca45bd7f63cabeba1bb0
nshooter3 <nick.shooter@gmail.com>
2016-03-09 19:13:48 -0500

    Added particle effect prefabs

---
+++ DemonGymnasium/Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestruct : MonoBehaviour {
+
+    public float timer = 0.2f;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
